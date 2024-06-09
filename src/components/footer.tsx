49841528d5bb48ffa3dbec2edbbd5f4b767e6848
Clark Brown <39170830+clarkedb@users.noreply.github.com>
2024-06-09 15:32:36 -0600

    add talks page (#11)

--- src/components/footer.tsx
+++ src/components/footer.tsx
@@ -4,7 +4,7 @@ export function Footer() {
   return (
     <footer className="mt-auto flex flex-row items-end px-2 py-4">
       <div className="fade w-100 relative mx-auto flex scroll-pr-6 flex-row text-sm tracking-tighter md:relative md:overflow-auto">
-        <p>Copyright Clark Brown, 2024</p>
+        <p>© 2024 - Clark Brown</p>
       </div>
     </footer>
   )

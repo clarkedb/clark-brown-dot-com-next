fd997cfb92c58c28ce6afc40d7db0d9967aa1427
Clark Brown <clark@clark-brown.com>
2024-08-02 23:14:10 -0600

    configure tailwind to use typography plugin

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,13 +1,30 @@
 import type { Config } from 'tailwindcss'
+import typography from '@tailwindcss/typography'
 
-const config: Config = {
+const config = {
   content: [
     './src/pages/**/*.{js,ts,jsx,tsx,mdx}',
     './src/components/**/*.{js,ts,jsx,tsx,mdx}',
     './src/app/**/*.{js,ts,jsx,tsx,mdx}',
     './src/hooks/**/*.{js,ts,jsx,tsx,mdx}',
   ],
-  plugins: [],
-}
+  theme: {
+    extend: {
+      fontFamily: {
+        sans: ['var(--font-geist-sans)'],
+        mono: ['var(--font-geist-mono)'],
+      },
+      typography: {
+        quoteless: {
+          css: {
+            'blockquote p:first-of-type::before': { content: 'none' },
+            'blockquote p:first-of-type::after': { content: 'none' },
+          },
+        },
+      },
+    },
+  },
+  plugins: [typography],
+} satisfies Config
 
 export default config

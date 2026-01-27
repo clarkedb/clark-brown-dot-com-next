e1c50d287e1e1ce0b9a646884bc045e32761b1e4
Clark Brown <39170830+clarkedb@users.noreply.github.com>
2026-01-26 21:40:29 -0700

    dynamic resume (#73)
    * clean up lockfile
    
    * make resume link redirect to r2 link
    
    * update sitemap
    
    * remove resume asset

--- next.config.js
+++ next.config.js
@@ -1,4 +1,12 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {}
+const nextConfig = {
+  redirects: () => [
+    {
+      source: '/resume',
+      destination: 'https://static.clark-brown.com/resume/resume.pdf',
+      permanent: true,
+    },
+  ],
+}
 
 module.exports = nextConfig

8866a47fb6ac5e452900aac874f848581ec72a31
Clark Brown <clark@clark-brown.com>
2026-01-29 14:15:51 -0700

    add legal pages

--- src/app/sitemap.ts
+++ src/app/sitemap.ts
@@ -9,7 +9,7 @@ export default function sitemap(): MetadataRoute.Sitemap {
   const siteUrl = process.env.NEXT_PUBLIC_SITE_URL || 'https://clark-brown.com'
 
   // FIXME: dates here are always today
-  const routes = ['', '/blog', '/talks']
+  const routes = ['', '/blog', '/talks', '/privacy', '/imprint']
   const routeEntries = routes.map((route) => ({
     url: `${siteUrl}${route}`,
     lastModified: new Date().toISOString().split('T')[0],

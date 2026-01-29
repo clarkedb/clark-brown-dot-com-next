e24a636a2d1dc7be0fd2e31f94fdbcdd38365af8
Clark Brown <clark@clark-brown.com>
2026-01-29 13:21:59 -0700

    update copyright date

--- src/components/footer.tsx
+++ src/components/footer.tsx
@@ -2,7 +2,7 @@ export function Footer() {
   return (
     <footer className="mt-auto flex flex-row items-end px-2 py-4">
       <div className="fade w-100 relative mx-auto flex scroll-pr-6 flex-row text-sm tracking-tighter md:relative md:overflow-auto">
-        <p>© 2024 - Clark Brown</p>
+        <p>© 2026 - Clark Brown</p>
       </div>
     </footer>
   )
